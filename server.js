71d2dd4a13c9de97d02c6331b79d5f78ea3f2956
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2022-08-30 00:31:47 +0300

    Fixing PORT

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const server = express();
 const Forecast = require("./Forecast.js");
+const PORT = process.env.PORT;
 
 server.use(cors());
 
 function ServerInit() {
+  server.listen(PORT, () => {
+    console.log(`Server is up and listening on PORT : ${PORT}`);
+  });
+
   server.get("*", (req, res) => {
     res.send("Error 404, Page Not Found");
   });
